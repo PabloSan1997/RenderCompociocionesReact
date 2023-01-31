30f338cb4febe3b4bd990ce0fd6d8e160bdf3eb4
PabloSan1997 <jos_pab_997@hotmali.com>
2023-01-30 19:00:58 -0600

    Primera subida

---
+++ src/App/AppUI.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { TodoContext } from "../TodoContext";
+import { TodoCounter } from "../TodoCounter";
+import { TodoSearch } from "../TodoSearch";
+import { TodoList } from "../TodoList";
+import { TodoItem } from "../TodoItem";
+import { TodosError } from "../TodosError";
+import { TodosLoading } from "../TodosLoading";
+import { EmptyTodos } from "../EmptyTodos";
+import { TodoForm } from "../TodoForm";
+import { CreateTodoButton } from "../CreateTodoButton";
+import { Modal } from "../Modal";
+import { Header } from "../TodoHeader";
+
+function AppUI() {
+  const {
+    error,
+    loading,
+    searchedTodos,
+    completeTodo,
+    deleteTodo,
+    openModal,
+    setOpenModal,
+    totalTodos,
+    completedTodos,
+    searchValue,
+    setSearchValue,
+  } = React.useContext(TodoContext);
+
+  return (
+    <React.Fragment>
+      <Header>
+      <TodoCounter totalTodos={totalTodos} completedTodos={completedTodos} />
+      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
+      </Header>
+
+      <TodoList>
+        {error && <TodosError />}
+        {loading && <TodosLoading />}
+        {!loading && !searchedTodos.length && <EmptyTodos />}
+
+        {searchedTodos.map((todo) => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
+        ))}
+      </TodoList>
+
+      {!!openModal && (
+        <Modal>
+          <TodoForm />
+        </Modal>
+      )}
+
+      <CreateTodoButton setOpenModal={setOpenModal} />
+    </React.Fragment>
+  );
+}
+
+export { AppUI };
---
+++ src/TodoHeader/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Header({children}){
+  return ( <header>
+        {children}
+    </header>);
+
+}
+export {Header}
